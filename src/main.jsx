4dc7bf33ef49251422ac446d48552913fe397a8e
grangerthebarber <grangerthebarber@gmail.com>
2026-06-08 06:17:43 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TheList from './the-list.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TheList />
+  </React.StrictMode>
+)
